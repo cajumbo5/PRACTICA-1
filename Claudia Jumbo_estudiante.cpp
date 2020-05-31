7698d881c7e42d73f53d9241cdd57cc52dae3302
Claudia Jumbo <cjumbomontalvan@gmail.com>
2020-05-31 13:26:58 -0500

    Mi primera practica Estructura Vacia de un Programa en C++

---
+++ Claudia Jumbo_estudiante.cpp
@@ -0,0 +1,19 @@
+#include <<stadio.h>>
+int fibonaci (int n)
+{
+  //Insercion de codigo
+   return 0;
+
+}
+int factorial(int p)
+{
+
+   //insercion de codigo
+   return 0;
+}
+main()
+{
+
+//funcion principal
+
+}
